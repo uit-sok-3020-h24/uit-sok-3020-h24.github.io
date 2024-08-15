85e48a276e3a59ea98ce59b196c0b74b9443a3a2
Oystein Myrland <omy000@uit.no>
2024-08-15 14:48:24 +0200

    Add files via upload

---
+++ my_first_exercise.R
@@ -0,0 +1,40 @@
+# From POE5 use the data file stockton5
+
+# sprice livarea beds baths lgelot age pool
+# 
+# Obs:   2610 home sales in Stockton, CA from Oct 1, 1996 to Nov 30, 1998
+# 
+# sprice         	selling price of home, dollars
+# livarea         living area, hundreds of square feet
+# beds            number of beds
+# baths           number of baths
+# lgelot          =1 if lot size > .5 acres, 0 otherwise
+# age            	age of home at time of sale, years
+# pool           	=1 if home has pool, 0 otherwise
+
+
+# Load the data
+
+
+# Make a plot of the data, with sprice on the y-axis and livarea on the x-axis
+# use ggplot2
+
+# Make the color of the points red
+# Add a title to the plot
+# Add a label to the x-axis
+# Add a label to the y-axis
+
+
+# Change the livarea variable to be in square meters instead of hundreds of square feet, call it livarea_m2
+
+# Make a plot of the data, with sprice on the y-axis and livarea_m2 on the x-axis
+
+# Add color to the points based on the value of the baths variable
+
+# Find the average selling price of homes with a pool and without a pool
+
+# Find the average selling price of homes with a lot size greater than .5 acres and less than .5 acres
+
+# Find the average selling price of homes with 1, 2, 3, 4, and 5+ bedrooms
+
+# Find the average selling price of homes with 1, 2 and 3 bedrooms against those with 4+ bedrooms
